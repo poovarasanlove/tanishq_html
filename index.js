b28981a118ca1dd89087c9aff367845c4dcec563
Poovarasan <poovarasan@Poovarasans-MacBook-Air.local>
2021-10-12 21:59:07 -0700

    finished the sit

--- index.js
+++ index.js
@@ -43,49 +43,70 @@
         var counting = setInterval(function() {
             var text = count != 0 ? count : "Go";
             $('.num-count').text(text);
-            count-- || (clearInterval(counting),$('.draw-count-content, .image-set-logo').fadeOut(1000),$('.canvas-blk, .canvas-blk .circle').fadeIn(1000),$('.canvas-blk, .canvas-blk .circle').addClass('active'));
+            count-- || (clearInterval(counting),$('.draw-count-content, .image-set-logo').fadeOut(1000),$('.canvas-blk, .canvas-blk .circle').fadeIn(1000),$('.canvas-blk, .canvas-blk .circle').addClass('active'),$('#sig-canvas').addClass('circle-draw'));
             // var text = (count1 == 0) ? 'Go': count1;
         }, 1000)
-        sequnce();        
+    });
+    $('#sig-canvas').on('touchend mousedown', function() {
+        if ($(this).hasClass('circle-draw') && !$(this).hasClass('disabel-circle')){
+            circle();
+            $(this).addClass('disabel-circle');
+        }else if ($(this).hasClass('square-draw') && !$(this).hasClass('disabel-square')){
+            square();
+            $(this).addClass('disabel-square');
+        }else if ($(this).hasClass('hexa-draw') && !$(this).hasClass('disabel-hexa')){
+            hexa();
+            $(this).addClass('disabel-hexa');
+        }
+
     });
 
-function sequnce() {
+function circle() {
     setTimeout(function () {
+        $('.help-text').find('h4').html('Nice! </br> Try another shape?');
+        $('.help-text').fadeIn();
+    },5000)
+    setTimeout(function () {
+        clearCanvas();
+        $('.help-text').fadeOut();
+        if (!$('.canvas-blk .circle').hasClass('done')) {
+            $('.canvas-blk .circle').addClass('done');
+            $('.canvas-blk .square').addClass('active');
+        }
+        $('#sig-canvas').removeClass('circle-draw disabel-circle');
+        $('#sig-canvas').addClass('square-draw');
+    },7000)
+};
+function square() {
+        setTimeout(function () {
+            $('.help-text').find('h4').html('Oh we like that.<br/> Try another?');
             $('.help-text').fadeIn();
-            $('.help-text').find('h4').html('Nice! </br> Try another shape?');
-        },12000)
+        },5000)
         setTimeout(function () {
-            clearCanvas();
-            $('.help-text').fadeOut();
-            if (!$('.canvas-blk .circle').hasClass('done')) {
-                $('.canvas-blk .circle').addClass('done');
-                $('.canvas-blk .square').addClass('active');
-            }
-        },15000)
-        setTimeout(function () {
-            clearCanvas();
             $('.help-text').fadeOut();
              if (!$('.canvas-blk .square').hasClass('done')) {
                 $('.canvas-blk .square').addClass('done');
                 $('.canvas-blk .hexagon').addClass('active');
             }
-        }, 25000);
-        setTimeout(function () {
-            $('.help-text').fadeIn();
-            $('.help-text').find('h4').html('Oh we like that.<br/> Try another?');
-        },23000)
-        setTimeout(function () {
+            $('#sig-canvas').removeClass('square-draw disabel-square');
+            $('#sig-canvas').addClass('hexa-draw');
             clearCanvas();
-            $('.help-text').fadeOut();
-            if (!$('.canvas-blk .hexagon').hasClass('done')) {
-                $('.canvas-blk .hexagon').addClass('done');
-                $('.end-layout').addClass('active');
-            }
-        }, 35000);
-        setTimeout(function () {
-            $('.help-text').fadeIn();
+        }, 7000);
+}
+function hexa() {
+setTimeout(function () {
             $('.help-text').find('h4').html('Pretty! <br/>Want to see the jewellery<br/>inspired by your karigari?');
-        },33000)
+            $('.help-text').fadeIn();
+        },5000)
+setTimeout(function () {
+        $('.help-text').fadeOut();
+        if (!$('.canvas-blk .hexagon').hasClass('done')) {
+            $('.canvas-blk .hexagon').addClass('done');
+            $('.end-layout').addClass('active');
+        }
+        $('#sig-canvas').removeClass('hexa-draw disabel-hexa');
+        clearCanvas();
+    }, 7000);
 }
    $(window).on('load', function() {
     var winwid = $(window).width();
@@ -95,178 +116,6 @@ function sequnce() {
         clearCanvas();
     }   
    });
-   // var width = $(window).width();
-// $(window).on('resize', function() {
-//   if ($(this).width() !== width && width < 1200) {
-//     width = $(this).width();
-//     $('#sig-canvas').attr('width', width);
-//     clearCanvas();
-//   }
-// });
-// function touchHandler(e) {
-//     "use strict";
-
-//     var el = e.target;
-
-//     if (el.parentNode.id === "sl-m") {
-//         if (e.type === "touchstart") {
-//             touch.startX = e.changedTouches[0].screenX;
-//             touch.startY = e.changedTouches[0].screenY;
-//         } else {
-//             touch.endX = e.changedTouches[0].screenX;
-//             touch.endY = e.changedTouches[0].screenY;
-
-//             touch.lenX = Math.abs(touch.endX - touch.startX);
-//             touch.lenY = Math.abs(touch.endY - touch.startY);
-
-//             if (touch.lenY < 20) {
-//                 // disable scroll
-//                 document.body.style.overflowY = "hidden";
-
-//                 // do swipe related stuff
-//                 swipe(el.parentNode);
-//             } else {
-//                 // enable scroll if swipe was not intended
-//                 document.body.style.overflowY = "scroll";
-//             }
-//         }
-//     } else {
-//         // keep scroll enabled if touch is outside the image slider 
-//         document.body.style.overflowY = "scroll";
-//     }
-// }
-            
-//             // Get a regular interval for drawing to the screen
-//             window.requestAnimFrame = (function (callback) {
-//                 return window.requestAnimationFrame || 
-//                             window.webkitRequestAnimationFrame ||
-//                             window.mozRequestAnimationFrame ||
-//                             window.oRequestAnimationFrame ||
-//                             window.msRequestAnimaitonFrame ||
-//                             function (callback) {
-//                                 window.setTimeout(callback, 1000/60);
-//                             };
-//             })();
-
-//             // Set up the canvas
-//             var canvas = document.getElementById("sig-canvas");
-//             var ctx = canvas.getContext("2d");
-//             ctx.strokeStyle = "#fff";
-//             ctx.lineWidth = 10;
-
-//             // Set up the UI
-//             // var sigText = document.getElementById("sig-dataUrl");
-//             // var sigImage = document.getElementById("sig-image");
-//             // var clearBtn = document.getElementById("sig-clearBtn");
-//             // var submitBtn = document.getElementById("sig-submitBtn");
-//             // clearBtn.addEventListener("click", function (e) {
-//             //     clearCanvas();
-//             //     sigText.innerHTML = "Data URL for your signature will go here!";
-//             //     sigImage.setAttribute("src", "");
-//             // }, false);
-//             // submitBtn.addEventListener("click", function (e) {
-//             //     var dataUrl = canvas.toDataURL();
-//             //     sigText.innerHTML = dataUrl;
-//             //     sigImage.setAttribute("src", dataUrl);
-//             // }, false);
-
-//             // Set up mouse events for drawing
-//             var drawing = false;
-//             var mousePos = { x:0, y:0 };
-//             var lastPos = mousePos;
-//             canvas.addEventListener("mousedown", function (e) {
-//                 drawing = true;
-//                 lastPos = getMousePos(canvas, e);
-//             }, false);
-//             canvas.addEventListener("mouseup", function (e) {
-//                 drawing = false;
-//             }, false);
-//             canvas.addEventListener("mousemove", function (e) {
-//                 mousePos = getMousePos(canvas, e);
-//             }, false);
-
-//             // Set up touch events for mobile, etc
-//             canvas.addEventListener("touchstart", function (e) {
-//                 mousePos = getTouchPos(canvas, e);
-//                 var touch = e.touches[0];
-//                 var mouseEvent = new MouseEvent("mousedown", {
-//                     clientX: touch.clientX,
-//                     clientY: touch.clientY
-//                 });
-//                 canvas.dispatchEvent(mouseEvent);
-//             }, false);
-//             canvas.addEventListener("touchend", function (e) {
-//                 var mouseEvent = new MouseEvent("mouseup", {});
-//                 canvas.dispatchEvent(mouseEvent);
-//             }, false);
-//             canvas.addEventListener("touchmove", function (e) {
-//                 var touch = e.touches[0];
-//                 var mouseEvent = new MouseEvent("mousemove", {
-//                     clientX: touch.clientX,
-//                     clientY: touch.clientY
-//                 });
-//                 canvas.dispatchEvent(mouseEvent);
-//             }, false);
-
-//             // Prevent scrolling when touching the canvas
-//             document.body.addEventListener("touchstart", function (e) {
-//                 if (e.target == canvas) {
-//                     e.preventDefault();
-//                 }
-//             }, false);
-//             document.body.addEventListener("touchend", function (e) {
-//                 if (e.target == canvas) {
-//                     e.preventDefault();
-//                 }
-//             }, false);
-//             document.body.addEventListener("touchmove", function (e) {
-//                 if (e.target == canvas) {
-//                     e.preventDefault();
-//                 }
-//             }, false);
-
-//             // Get the position of the mouse relative to the canvas
-//             function getMousePos(canvasDom, mouseEvent) {
-//                 var rect = canvasDom.getBoundingClientRect();
-//                 return {
-//                     x: mouseEvent.clientX - rect.left,
-//                     y: mouseEvent.clientY - rect.top
-//                 };
-//             }
-
-//             // Get the position of a touch relative to the canvas
-//             function getTouchPos(canvasDom, touchEvent) {
-//                 var rect = canvasDom.getBoundingClientRect();
-//                 return {
-//                     x: touchEvent.touches[0].clientX - rect.left,
-//                     y: touchEvent.touches[0].clientY - rect.top
-//                 };
-//             }
-
-//             // Draw to the canvas
-//             function renderCanvas() {
-//                 if (drawing) {
-//                     ctx.moveTo(lastPos.x, lastPos.y);
-//                     ctx.lineTo(mousePos.x, mousePos.y);
-//                     ctx.stroke();
-//                     lastPos = mousePos;
-//                 }
-//             }
-
-//             // Clear the canvas
-//             function clearCanvas() {
-//                 canvas.width = canvas.width;
-//             }
-
-//             // Allow for animation
-//             (function drawLoop () {
-//                 requestAnimFrame(drawLoop);
-//                 renderCanvas();
-//             })();
-
-//         // })();
-
-
 
 
 var canvas = document.getElementById("sig-canvas");
@@ -369,39 +218,6 @@ window.addEventListener('touchmove', ev => {
     ev.stopImmediatePropagation();
   };
 }, { passive: false });
-
-
-
-// document.body.addEventListener("touchstart", function (e) {
-//     // if (body.isSwipe(canvas) && e.target) {
-//     //    e.preventDefault();
-//     //    e.stopPropagation();
-//     //    swiping = true;
-//     // }
-//   if (e.target == canvas) {
-//     e.preventDefault();
-//   }
-// }, false);
-// document.body.addEventListener("touchend", function (e) {
-//   // if (body.isSwipe(canvas) && e.target) {
-//   //      e.preventDefault();
-//   //      e.stopPropagation();
-//   //      swiping = true;
-//   //   }
-//   if (e.target == canvas) {
-//     e.preventDefault();
-//   }
-// }, false);
-// document.body.addEventListener("touchmove", function (e) {
-//     if (e.target == canvas) {
-//     e.preventDefault();
-//   }
-//   // if (body.isSwipe(canvas) && e.target) {
-//   //      e.preventDefault();
-//   //      e.stopPropagation();
-//   //      swiping = true;
-//   //   }
-// }, false);
 
 function clearCanvas() {
     canvas.width = canvas.width;
